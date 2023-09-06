727bbc8ca1b15ba2e73931a47d40a386deab2246
mikey <michael.kiernan@healthengine.com.au>
2023-09-06 20:58:12 +0800

    don't abort on error

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  /**
+   * By default, if any error happens while creating the
+   * application your app will exit with the code 1.
+   *
+   * If you want to make it throw an error instead disable
+   * the option abortOnError.
+   */
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
   await app.listen(3000);
 }
 bootstrap();
